1478a7301096126eee70de13b78e7855c7ef6785
Gabe Saba <gabesaba@gmail.com>
2020-01-17 23:56:27 -0500

    Implement Interpreter

---
+++ src/interpreter/interpreter.kt
@@ -0,0 +1,50 @@
+package interpreter
+
+import functions.car
+import functions.cdr
+import functions.cons
+import syntax.Expression
+import syntax.buildSyntaxTree
+import syntax.tokenize
+import types.Atom
+import types.Fn
+import types.Type
+
+class Interpreter() {
+    var env = mutableMapOf(
+        "cons" to cons,
+        "car" to car,
+        "cdr" to cdr
+    )
+
+    fun parseAndEval(inp: String): String{
+        val tokens = tokenize(inp)
+        val tree = buildSyntaxTree(tokens)
+        return eval(tree).toString()
+    }
+
+    private fun eval(exp: Expression): Type {
+        when (exp) {
+            is Expression.S -> {
+                val exps = exp.expressions
+                val fn = eval(exps[0])
+                val args = exps.subList(1, exps.lastIndex + 1).map { eval(it) }
+                return when (fn) {
+                    is Fn -> {
+                        fn.f(args)
+                    }
+                    else -> {
+                        error("Expecting function as first argument in S-Expression")
+                    }
+                }
+            }
+            is Expression.Leaf -> {
+                return when {
+                    Atom.isAtom(exp.identifier) -> Atom(exp.identifier)
+                    env.containsKey(exp.identifier.toLowerCase()) -> env[exp.identifier.toLowerCase()]!!
+                    else -> error("Error: ${exp.identifier} is not in scope")
+                }
+            }
+        }
+    }
+}
